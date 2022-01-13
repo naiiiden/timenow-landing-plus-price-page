c7f1da04dd80a4e1080fdd65d65a2cdb08232db6
naiden <na4ko359@gmail.com>
2022-01-13 18:03:37 +0200

    90% completion commit in case i screw up

--- script/pricing page/script.js
+++ script/pricing page/script.js
@@ -1,3 +1,7 @@
 document.getElementById("hamburger").addEventListener("click", () => {
     document.getElementById("nav-ul").classList.toggle("show");
 });
+
+document.getElementById("toggle").addEventListener("change", () => {
+    document.getElementById("change").classList.toggle("change_prices");
+})
