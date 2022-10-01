cd99770481ac50abfd7b5fde7bf862edeaeaae6d
naiiiden <na4ko359@gmail.com>
2022-10-01 20:51:23 +0300

    toggle input add label

--- script/pricing page/script.js
+++ script/pricing page/script.js
@@ -4,4 +4,9 @@ document.getElementById("hamburger").addEventListener("click", () => {
 
 document.getElementById("toggle").addEventListener("click", () => {
     document.getElementById("change").classList.toggle("change_prices");
+    if (document.querySelector(".sr-only").textContent == "Billed Yearly") {
+        document.querySelector(".sr-only").textContent = "Billed Monthly";
+    } else {
+        document.querySelector(".sr-only").textContent = "Billed Yearly";
+    }
 })
